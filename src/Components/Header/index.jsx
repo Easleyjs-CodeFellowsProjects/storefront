1e5203c5351f450b7d8b8a9839402a807741ac2a
Josh Easley <easleyjs@gmail.com>
2023-11-09 21:48:41 -0800

    Adds initial header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,2 +1,34 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-/* will use App Bar from MUI */
+/*
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+*/
+
+export default function Header() {
+    return (
+      <Box sx={{ flexGrow: 12 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              OUR STORE
+            </Typography>
+            <Button color="inherit">CART (0)</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
